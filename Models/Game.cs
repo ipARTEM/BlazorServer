294f02d3451860d4552e4cf891b7f7dfe21aa7ff
Artem <a.khimin@yandex.ru>
2021-06-16 21:01:22 +0300

    Сделал компонент Razor

---
+++ Models/Game.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorServer.Models
+{
+    public class Game
+    {
+        public string Name { get; set; }
+
+        public string Genre { get; set; }
+
+        public DateTime ReleaseDate { get; set; }
+
+    }
+}
